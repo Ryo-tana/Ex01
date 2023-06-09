18e9e7cf564c1e1d7569a972947247ab6b9b3e8f
Ryo-tana <136040546+Ryo-tana@users.noreply.github.com>
2023-06-09 12:11:26 +0900

    Greetings added

--- coin.c
+++ coin.c
@@ -3,14 +3,8 @@
 #include <time.h>
 #include <unistd.h>
 int main(void){
-        srand(time(NULL));
         printf("What is your name?\n> ");
         char name[10];
         scanf("%s",name);
         printf("Hello, %s!\n",name);
-        printf("Rolling dice...\n");
-        int d1 = rand()%6+1;
-        sleep(1);
-        int d2 = rand()%6+1;
-        printf("Die 1: %d\nDie 2: %d\nTotal value: %d\n",d1,d2,d1+d2);
 }
