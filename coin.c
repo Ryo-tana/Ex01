9a25ec24e99108e5db34e492f2b558ecdf04bf86
Ryo-tana <136040546+Ryo-tana@users.noreply.github.com>
2023-06-09 11:35:19 +0900

    Dice game: initial revision

---
+++ coin.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+int main(void){
+				srand(time(NULL));
+  			printf("Rolling dice...\n");
+        int d1 = rand()%6+1;
+        sleep(1);
+        int d2 = rand()%6+1;
+        printf("Die 1: %d\nDie 2: %d\nTotal value: %d\n",d1,d2,d1+d2);
+}
