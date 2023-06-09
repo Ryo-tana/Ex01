f69f51bfff517ccc91a7068adbfbd3bece70f619
Ryo-tana <136040546+Ryo-tana@users.noreply.github.com>
2023-06-09 11:56:55 +0900

    Won/lost message added

--- coin.c
+++ coin.c
@@ -3,10 +3,15 @@
 #include <time.h>
 #include <unistd.h>
 int main(void){
-				srand(time(NULL));
-  			printf("Rolling dice...\n");
+        srand(time(NULL));
+        printf("Rolling dice...\n");
         int d1 = rand()%6+1;
         sleep(1);
         int d2 = rand()%6+1;
         printf("Die 1: %d\nDie 2: %d\nTotal value: %d\n",d1,d2,d1+d2);
+        if((d1+d2)>7){
+                printf("You won\n");
+        }else{
+                printf("You lost\n");
+        }
 }
